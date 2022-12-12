d78b3dc9d876e12c26e2e3bcc72875a726905fe6
rlagudtn <rlagudtn4510@naver.com>
2022-12-12 10:45:29 +0900

    init

---
+++ src/main/java/GoGetters/jenkinsproject/JenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package GoGetters.jenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectApplication.class, args);
+	}
+
+}
